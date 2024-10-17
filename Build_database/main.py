60e1ac7ed24d94ca95d9e777585b8d025ecf5823
FernandoRDLL <143847785+FernandoRDLL@users.noreply.github.com>
2024-10-17 20:15:41 +0200

    Update main.py

--- Build_database/main.py
+++ Build_database/main.py
@@ -1,7 +1,7 @@
 from aux_functions import *
 end_date = pl.datetime(2023, 12, 31)
 setup_folder_structure()
-download_raw_data_tables(username = "ffrdll", password = "cyqrys-tamgan-0pyMbi")
+download_raw_data_tables(username = "username", password = "password")
 gen_raw_data_dfs()
 prepare_comp_sf('both')
 prepare_crsp_sf('m')
