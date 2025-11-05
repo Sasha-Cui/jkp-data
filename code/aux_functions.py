78ca5c12f6e8b541a7be73334cd52b7c5632ca74
FernandoRDLL <143847785+FernandoRDLL@users.noreply.github.com>
2025-11-04 21:07:52 -0500

    Update aux_functions.py

--- code/aux_functions.py
+++ code/aux_functions.py
@@ -3269,6 +3269,7 @@ def market_returns(data_path, freq, wins_comp, wins_data_path):
             dolvol_lag1=col("dolvol").shift(1).over("id"),
         )
     )
+    __common_stocks.sink_parquet("common_stocks1.parquet")
     if wins_comp == 1:
         __common_stocks = add_cutoffs_and_winsorize(
             __common_stocks, wins_data_path, group_vars, dt_col
@@ -3277,10 +3278,12 @@ def market_returns(data_path, freq, wins_comp, wins_data_path):
     __common_stocks = apply_stock_filter_and_compute_indexes(
         __common_stocks, dt_col, max_date_lag
     )
+    __common_stocks.sink_parquet("common_stocks3.parquet")
     if freq == "d":
         __common_stocks = drop_non_trading_days(
             __common_stocks, "stocks", dt_col, ["excntry", "year", "month"], 0.25
         )
+    __common_stocks.sink_parquet("common_stocks4.parquet")
     __common_stocks.sort(["excntry", dt_col]).collect().write_parquet(
         f"market_returns{path_aux}.parquet"
     )
